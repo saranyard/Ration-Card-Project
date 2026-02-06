b7e1ad930de7a415d29d620c018f175a321acade
saranyard <saranya8844deva@gmail.com>
2026-02-06 19:55:23 +0530

    first upload

---
+++ ration.js
@@ -0,0 +1,136 @@
+function home() {
+    window.location.href = "home.html"
+}
+
+function login() {
+    window.location.href = "login.html"
+}
+
+function newuser() {
+    window.location.href = "newuser.html"
+}
+
+// function check() {
+//     window.location.href = "check.html"
+// }
+
+function applicantdetails() {
+    window.location.href = "applicantdetails.html"
+}
+
+
+function addressdetails() {
+
+    window.location.href = "addressdetails.html"
+}
+
+
+function familydetails() {
+
+    window.location.href = "familydetails.html"
+}
+
+
+function upload() {
+
+    window.location.href = "upload.html"
+}
+
+
+
+
+function review() {
+
+    window.location.href = "review.html"
+}
+
+
+
+function aknow() {
+
+    window.location.href = "acknowledgment.html"
+}
+
+
+function download() {
+
+    window.location.href = "download.html"
+}
+
+
+function trackapp() {
+
+    window.location.href = "trackapp.html"
+}
+
+
+function viewstatus() {
+
+    window.location.href = "viewstatus.html"
+}
+
+
+function pri() {
+
+    document.getElementById("down").style.display = "block";
+}
+
+
+
+function otp() {
+
+    if (document.getElementById("otp").style.display == "block") {
+        document.getElementById("otp").style.display = "none";
+
+    }
+    else {
+
+        document.getElementById("otp").style.display = "block";
+    }
+}
+
+
+
+
+function editapp()
+{
+    window.location.href="editapp.html"
+}
+
+
+function dashboard() {
+
+    window.location.href = "dashboard.html"
+}
+
+
+
+
+function profile() {
+
+    window.location.href = "profile.html"
+}
+
+
+
+function changeColor(button)
+{
+button.style.backgroundColor="#373C7D";
+button.style.color="white";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
